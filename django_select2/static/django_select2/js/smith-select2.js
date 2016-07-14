9f3f4b6928d5c4e158738be09fb1f9f4c3af380b
Nicolas Lara <nicolaslara@gmail.com>
2016-07-14 15:09:05 +0200

    reintroducing jquery10

--- django_select2/static/django_select2/js/smith-select2.js
+++ django_select2/static/django_select2/js/smith-select2.js
@@ -36,7 +36,7 @@ the specific language governing permissions and limitations under the Apache Lic
             }
         });
     }
-})(jQuery);
+})(jQuery10);
 
 (function ($, undefined) {
     "use strict";
@@ -3534,4 +3534,4 @@ the specific language governing permissions and limitations under the Apache Lic
         }
     };
 
-}(jQuery));
+}(jQuery10));
